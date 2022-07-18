7a62c4c3ce88a9b523434057b9efc0482cc3a394
Jona <mr-jona@hotmail.com>
2022-07-18 12:56:44 +0200

    wtf

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 const Homepage = () => {
-  const [dieNumber, setDieNumber] = useState(0);
   const [playerOne, setPlayerOne] = useState({
     name: "Player 1",
     score: 0,
@@ -18,43 +17,66 @@ const Homepage = () => {
     active: false,
     currentNum: 0,
   });
+  const [val, setVal] = useState(0);
 
-  const die = () => {
-    //calculate the number of the rolled die
-    const num = Math.floor(Math.random() * 6) + 1;
-
-    //assign the num to the correct player
-    if (playerOne.active) {
-      setPlayerOne.currentNum = num;
-    } else {
-      setPlayerTwo.currentNum = num;
-    }
-
+  const activeToggle = () => {
     //toggle active prop for each player
-    if (playerOne.active) {
-      setPlayerTwo({ ...playerTwo, active: true });
-      setPlayerOne({ ...playerOne, active: false });
-    }
-
-    if (playerTwo.active) {
-      setPlayerTwo({ ...playerTwo, active: false });
-      setPlayerOne({ ...playerOne, active: true });
-    }
+    // if (playerOne.active) {
+    //   setPlayerTwo({ ...playerTwo, active: true });
+    //   return setPlayerOne({ ...playerOne, active: false });
+    // } });
+    // setPlayerOne({ ...playerOne, active: !playerOne.active });
+    setPlayerTwo({ ...playerTwo, active: !playerTwo.active });
   };
 
-  useEffect(() => {
-    const compare = () => {
-      if (playerOne.currentNum > playerTwo.currentNum) {
-        return setPlayerOne({ ...playerOne, score: playerOne.score + 1 });
-      }
+  const die = (e) => {
+    //calculate the number of the rolled die
+    const num = Math.floor(Math.random() * 6) + 1;
+    setVal(num);
 
-      if (playerTwo.currentNum > playerOne.currentNum) {
-        return setPlayerTwo({ ...playerTwo, score: playerTwo.score + 1 });
-      }
-    };
+    if (e.target.textContent === "p1 die") {
+      const newP1 = { ...playerOne, currentNum: val };
+      console.log("if active", num);
+      setPlayerOne(newP1);
+    }
 
-    compare();
-  }, [playerOne.currentNum, playerTwo.currentNum]);
+    if (e.target.textContent === "p2 die") {
+      const newP2 = { ...playerTwo, currentNum: val };
+      setPlayerTwo(newP2);
+    }
+    //assign the num to the correct player
+
+    // if (playerOne.active) {
+    //   const newP1 = { ...playerOne, currentNum: val };
+    //   console.log("if active", num);
+    //   setPlayerOne(newP1);
+    // }
+    // if (playerTwo.active) {
+    //   const newP2 = { ...playerTwo, currentNum: val };
+    //   setPlayerTwo(newP2);
+    // }
+  };
+
+  const btnHandler = (e) => {
+    die(e);
+    activeToggle();
+  };
+
+  //   useEffect(() => {
+  //     const compare = () => {
+  //       if (playerOne.currentNum > playerTwo.currentNum) {
+  //         return setPlayerOne({ ...playerOne, score: playerOne.score + 1 });
+  //       }
+
+  //       if (playerTwo.currentNum > playerOne.currentNum) {
+  //         return setPlayerTwo({ ...playerTwo, score: playerTwo.score + 1 });
+  //       }
+  //     };
+
+  //     compare();
+  //   }, [playerOne.currentNum, playerTwo.currentNum]);
+  console.log(playerOne);
+  console.log(playerTwo);
 
   return (
     <div className="homepage">
@@ -65,19 +87,28 @@ const Homepage = () => {
           <p>score: {playerOne.score}</p>
           <p>wins: {playerOne.wins}</p>
           <p>losses: {playerOne.losses}</p>
-          {playerOne.active ? <button onClick={die}>p1 die</button> : null}
+          <p>current roll: {playerOne.currentNum}</p>
+          <p>active state: {String(playerOne.active)}</p>
+          {/* {playerOne.active ? (
+            <button onClick={btnHandler}>p1 die</button>
+          ) : null} */}
+          <button onClick={btnHandler}>p1 die</button>
         </div>
         <div className="player-2">
           <h3>{playerTwo.name}</h3>
           <p>score: {playerTwo.score}</p>
           <p>wins: {playerTwo.wins}</p>
           <p>losses: {playerTwo.losses}</p>
-          {playerTwo.active ? <button onClick={die}>p2 die</button> : null}
+          <p>current roll: {playerTwo.currentNum}</p>
+          <p>active state: {String(playerTwo.active)}</p>
+
+          {/* {playerTwo.active ? (
+            <button onClick={btnHandler}>p2 die</button>
+          ) : null} */}
+          <button onClick={btnHandler}>p2 die</button>
         </div>
       </div>
-      <div className="die-result">
-        <h1>{dieNumber}</h1>
-      </div>
+      <div className="die-result"></div>
     </div>
   );
 };
